7eb9cdbc1ba5e73fcb7c60540f30305cce9f73ff
Coffeeteer <tgrieshiemer@gmail.com>
2016-12-02 14:29:27 -0500

    tic-tac-toe game complete

--- js/script.js
+++ js/script.js
@@ -15,8 +15,84 @@ $(document).ready(function(){
 	var spot9 = $('#spot9');
 
 	$('#board li').on('click', function(){
-		if() {
+		if(spot1.hasClass('o') &&spot2.hasClass('o') && spot3.hasClass('o') ||
+			spot4.hasClass('o') &&spot5.hasClass('o') && spot6.hasClass('o') ||
+			spot7.hasClass('o') &&spot8.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') &&spot4.hasClass('o') && spot7.hasClass('o') ||
+			spot2.hasClass('o') &&spot5.hasClass('o') && spot8.hasClass('o') ||
+			spot3.hasClass('o') &&spot6.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') &&spot5.hasClass('o') && spot9.hasClass('o') ||
+			spot3.hasClass('o') &&spot5.hasClass('o') && spot7.hasClass('o')	
+			) {
+			alert('Winner is o');
+			$('#board li').text('+');
+			$('#board li').removeClass('disable');
+			$('#board li').text('o');
+			$('#board li').text('x');
+		} else if (spot1.hasClass('x') &&spot2.hasClass('x') && spot3.hasClass('x') ||
+			spot4.hasClass('x') &&spot5.hasClass('x') && spot6.hasClass('x') ||
+			spot7.hasClass('x') &&spot8.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') &&spot4.hasClass('x') && spot7.hasClass('x') ||
+			spot2.hasClass('x') &&spot5.hasClass('x') && spot8.hasClass('x') ||
+			spot3.hasClass('x') &&spot6.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') &&spot5.hasClass('x') && spot9.hasClass('x') ||
+			spot3.hasClass('x') &&spot5.hasClass('x') && spot7.hasClass('x')	
+			) {
+			alert('Winner is x');
+			$('#board li').text('+');
+			$('#board li').removeClass('disable');
+			$('#board li').text('o');
+			$('#board li').text('x');
+		} else if (turns == 9) {
+			alert('Tie Game.');
+			$('#board li').text('+');
+			$('#board li').removeClass('disable');
+			$('#board li').text('o');
+			$('#board li').text('x');
+			turns = 0;
+		} else if($(this).hasClass('disable'))	{
+			alert('This spot is already filled.');
+		} else if(turns%2 == 0) {
+			turns++;
+			$(this).text(o);
+			$(this).addClass('disable o');
+			if(spot1.hasClass('o') &&spot2.hasClass('o') && spot3.hasClass('o') ||
+			spot4.hasClass('o') &&spot5.hasClass('o') && spot6.hasClass('o') ||
+			spot7.hasClass('o') &&spot8.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') &&spot4.hasClass('o') && spot7.hasClass('o') ||
+			spot2.hasClass('o') &&spot5.hasClass('o') && spot8.hasClass('o') ||
+			spot3.hasClass('o') &&spot6.hasClass('o') && spot9.hasClass('o') ||
+			spot1.hasClass('o') &&spot5.hasClass('o') && spot9.hasClass('o') ||
+			spot3.hasClass('o') &&spot5.hasClass('o') && spot7.hasClass('o')	
+			) {
+				alert('Winner o');	
+				turns = 0;
+			  }
+			} else {
+			turns++;
+			$(this).text(x);
+			$(this).addClass('disable x');
+			if(spot1.hasClass('x') &&spot2.hasClass('x') && spot3.hasClass('x') ||
+			spot4.hasClass('x') &&spot5.hasClass('x') && spot6.hasClass('x') ||
+			spot7.hasClass('x') &&spot8.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') &&spot4.hasClass('x') && spot7.hasClass('x') ||
+			spot2.hasClass('x') &&spot5.hasClass('x') && spot8.hasClass('x') ||
+			spot3.hasClass('x') &&spot6.hasClass('x') && spot9.hasClass('x') ||
+			spot1.hasClass('x') &&spot5.hasClass('x') && spot9.hasClass('x') ||
+			spot3.hasClass('x') &&spot5.hasClass('x') && spot7.hasClass('x')	
+			) {
+				alert('Winner x');	
+				turns = 0;	
+			}
+		}	
+	});
 
-		}
+	// Reest Handler
+	$('#reset').on('click', function() {
+		$('#board li').text('+');
+		$('#board li').removeClass('disable');
+		$('#board li').text('o');
+		$('#board li').text('x');
+		turns = 0;
 	});
 });
